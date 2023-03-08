bcafb9bf3ffec09889089f39efda88d3a793d9ea
Daniel-Digid <daniel@dig.id>
2023-03-08 15:38:46 +0000

    dev

--- template-parts/pages/jura/top-of-jura/section-intro.php
+++ template-parts/pages/jura/top-of-jura/section-intro.php
@@ -3,7 +3,7 @@
     $header_imgArr = wp_get_attachment_image_src( $header_image, 'full' ); ?>
     <div class="bg-cover bg-center h-full w-full" style="background-image: url(<?php echo $header_imgArr[0]; ?>);"></div>
     <div class="flex justify-center h-full w-full bg-no-repeat" style="background-image: url(<?php echo wp_upload_dir()['url'] . '/jura_mountain.svg' ?>);    background-position: 0 218%;">
-    <div class="py-20 container flex grid grid-cols-3 justify-center">
+    <div class="py-20 container grid grid-cols-3 justify-center">
         <div class="mb-4">
             <h2 class="text-black text-6xl font-bold uppercase"><?php the_field('section_intro_title'); ?></h2>
         </div>
