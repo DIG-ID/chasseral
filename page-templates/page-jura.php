f0c18f95ab330065b6bd2f8c8bf6255f666dacdb
Daniel-Digid <daniel@dig.id>
2023-03-08 16:38:29 +0000

    dev

--- page-templates/page-jura.php
+++ page-templates/page-jura.php
@@ -7,6 +7,6 @@ get_header( 'jura' );
 	do_action( 'before_main_content' );
 		//get_template_part( 'template-parts/loops/loop', 'subpages' );
 		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'intro' );
-		get_template_part( 'template-parts/modules/sections/section', '2cols-image' );
+		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'der-chasseral' );
 	do_action( 'after_main_content' );
 get_footer();
