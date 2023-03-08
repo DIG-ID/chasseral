ae256d2d34207a46d59b04d3813ca8b61b415857
Daniel-Digid <daniel@dig.id>
2023-03-08 15:28:05 +0000

    dev

--- page-templates/page-jura.php
+++ page-templates/page-jura.php
@@ -5,6 +5,7 @@
 
 get_header( 'jura' );
 	do_action( 'before_main_content' );
-		get_template_part( 'template-parts/loops/loop', 'subpages' );
+		//get_template_part( 'template-parts/loops/loop', 'subpages' );
+		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'intro' );
 	do_action( 'after_main_content' );
 get_footer();
