fa74fcfb708865bdb547c6a38e6074cab40f69bf
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-08 17:59:03 +0000

    up

--- template-parts/pages/home/sub-pages.php
+++ template-parts/pages/home/sub-pages.php
@@ -1,7 +1,7 @@
 <section class="section-sub-pages grid gap-5 grid-cols-3">
 	<?php
-	do_action( 'sub_pages_link', 'top_of_jura' );
-	do_action( 'sub_pages_link', 'hotel_restaurant' );
 	do_action( 'sub_pages_link', 'szl' );
+	do_action( 'sub_pages_link', 'hotel_restaurant' );
+	do_action( 'sub_pages_link', 'top_of_jura' );
 	?>
 </section>
