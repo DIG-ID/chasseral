83ade9df947db49c5cd2a417b0064f70941229e9
Daniel-Digid <daniel@dig.id>
2023-03-10 16:26:36 +0000

    dev

--- page-templates/page-jura.php
+++ page-templates/page-jura.php
@@ -10,5 +10,6 @@ get_header( 'jura' );
 		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'der-chasseral' );
 		get_template_part( 'template-parts/modules/activities' );
 		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'perfekt-fur' );
+		get_template_part( 'template-parts/pages/jura/top-of-jura/section', 'institutionen' );
 	do_action( 'after_main_content' );
 get_footer();
---
+++ template-parts/pages/jura/top-of-jura/section-institutionen.php
@@ -0,0 +1,5 @@
+<section class="section-institutionen px-0 py-32">
+    <div class="flex justify-center p-20 flex-col">
+        
+    </div>
+</section>
