5d9989b5e91b90eb6fa319c6080c16993dd18d1e
Daniel-Digid <daniel@dig.id>
2023-03-19 18:57:28 +0000

    dev

--- template-parts/posts/zimmer/section-content.php
+++ template-parts/posts/zimmer/section-content.php
@@ -20,12 +20,12 @@
         </div>
 	</div>
     <div class="container mx-auto max-w-7xl px-8 mt-10">
-            <?php $page = get_permalink( get_page_by_path('hotel-restaurant/zimmer') -> ID ); ?>
-            <a href="<?php echo $page; ?>">
-                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mr-2">
-                    <path d="M10.9885 9.6065e-07L12.9458 1.925L5.25385 9.625L21.977 9.625L21.977 12.375L5.25385 12.375L12.9458 20.075L10.9885 22L-5.81821e-05 11L10.9885 9.6065e-07Z" fill="#9C9C9C"/>
-                </svg>
-                <span class="text-sm text-[#9C9C9C]"><?php echo esc_html( 'Zurück zur Übersicht', 'chasseral' ) ?></span>
-            </a>
+        <?php $page = get_permalink( get_page_by_path('hotel-restaurant/zimmer') -> ID ); ?>
+        <a href="<?php echo $page; ?>">
+            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="inline mr-2">
+                <path d="M10.9885 9.6065e-07L12.9458 1.925L5.25385 9.625L21.977 9.625L21.977 12.375L5.25385 12.375L12.9458 20.075L10.9885 22L-5.81821e-05 11L10.9885 9.6065e-07Z" fill="#9C9C9C"/>
+            </svg>
+            <span class="text-sm text-[#9C9C9C]"><?php echo esc_html( 'Zurück zur Übersicht', 'chasseral' ) ?></span>
+        </a>
     </div>
 </section>
