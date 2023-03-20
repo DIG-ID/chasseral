a3208b700d824e41e299b595431caec72aed2944
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-20 12:07:04 +0000

    up

--- template-parts/pages/szl/intro.php
+++ template-parts/pages/szl/intro.php
@@ -3,10 +3,10 @@
 		<h1 class="text-6xl font-bold uppercase mb-10"><?php the_field( 'intro_title' ); ?></h1>
 		<div class="szl-logos w-full flex">
 			<?php
-			$images = get_field( 'intro_szl_logos' );
-			if ( $images ) :
-				foreach ( $images as $image ) :
-					?><img src="<?php echo esc_url( $image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( $image['alt']) ; ?>" /><?php
+			$szl_logos = get_field( 'intro_szl_logos' );
+			if ( $szl_logos ) :
+				foreach ( $szl_logos as $logo ) :
+					?><img class="max-w-[180px] h-auto object-cover" src="<?php echo wp_get_attachment_image_url( $logo, 'full' ); ?>" alt="<?php echo wp_get_attachment_caption( $logo ); ?>" /><?php
 				endforeach;
 			endif;
 			?>
