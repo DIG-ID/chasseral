ac3ee0f0921add1d7d499ed4afdf4ea1da6791ee
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-20 17:31:09 +0000

    up

--- template-parts/pages/home/about.php
+++ template-parts/pages/home/about.php
@@ -1,4 +1,4 @@
-<section class="section-about pt-36 pb-[90vh] bg-contain bg-no-repeat bg-bottom overflow-hidden" style="background-image: url(<?php echo esc_url( wp_get_attachment_image_url( get_field( 'about_background_image' ), 'full' ) ); ?>);">
+<section class="section-about pt-36 pb-[70vh] bg-contain bg-no-repeat bg-bottom overflow-hidden" style="background-image: url(<?php echo esc_url( wp_get_attachment_image_url( get_field( 'about_background_image' ), 'full' ) ); ?>);">
 	<div class="container mx-auto px-8 grid grid-cols-1 lg:grid-cols-12">
 		<div class="col-span-1 lg:col-span-4">
 			<h2 class="section-title section-title text-3xl mb-16"><?php the_field( 'about_title' ); ?></h2>
