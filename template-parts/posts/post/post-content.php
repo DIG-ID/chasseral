f936713ef3430d348986b16528aa806a2d82e51d
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-22 19:02:44 +0000

    up

--- template-parts/posts/post/post-content.php
+++ template-parts/posts/post/post-content.php
@@ -1,10 +1,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'overflow-hidden py-8 lg:py-20' ); ?>>
 	<div class="container mx-auto px-8 py-36 grid grid-cols-1 lg:grid-cols-12">
 		<div class="post-content-wrapper col-span-10">
-			<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished"><?php echo get_the_date( 'l d, F Y' ); ?></time>
+			<time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="block font-sans text-sm text-text-grey font-normal whitespace-nowrap mb-2"><?php echo get_the_date(); ?></time>
 			<h1 class="title-normal"><?php the_title(); ?></h1>
-			<figure class="mb-16 post__image">
+			<figure class="mb-16 post__image relative">
 				<?php
+				$badge_label = get_field( 'category_badge_label', 'category_' . $post_cat_id );
+				if ( $badge_label ) :
+					?><p class="badge text-white text-[13px] font-semibold py-1 px-3 mb-10 absolute top-5 left-5 uppercase" style="background-color: <?php do_action( 'cat_color', 'hex' ); ?>;"><?php echo esc_html( $badge_label ); ?></p><?php
+				endif;
 				if ( has_post_thumbnail() ) :
 					the_post_thumbnail( 'full' );
 				else :
