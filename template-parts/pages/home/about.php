a9fb40e47c5aac0b96c4b26bd49f1ecab8059742
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-22 16:59:46 +0000

    up

--- template-parts/pages/home/about.php
+++ template-parts/pages/home/about.php
@@ -1,7 +1,7 @@
 <section class="section-about pt-16 lg:pt-36 pb-[20vh] lg:pb-[70vh] bg-contain bg-no-repeat bg-bottom overflow-hidden" style="background-image: url(<?php echo esc_url( wp_get_attachment_image_url( get_field( 'about_background_image' ), 'full' ) ); ?>);">
 	<div class="container mx-auto px-8 grid grid-cols-1 lg:grid-cols-12">
 		<div class="col-span-1 lg:col-span-4">
-			<h2 class="title-normal font-normal"><?php the_field( 'about_title' ); ?></h2>
+			<h2 class="title-normal !font-normal"><?php the_field( 'about_title' ); ?></h2>
 		</div>
 		<div class="col col-span-1 lg:col-span-5 lg:col-start-6">
 			<p class="text-body"><?php the_field( 'about_description' ); ?></p>
--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,7 +1,7 @@
 <section class="section-intro py-16 lg:py-32 overflow-hidden">
 	<div class="container mx-auto px-8 grid grid-cols-1 lg:grid-cols-12">
 		<div class="col-span-1 lg:col-span-4">
-			<h2 class="title-normal font-normal"><?php the_field( 'intro_title' ); ?></h2>
+			<h2 class="title-normal !font-normal"><?php the_field( 'intro_title' ); ?></h2>
 		</div>
 		<div class="col col-span-1 lg:col-span-5 lg:col-start-6">
 			<p class="text-body"><?php the_field( 'intro_description' ); ?></p>
