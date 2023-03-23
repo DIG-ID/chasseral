6048c7dcfe9f7b269e7d364fb230333b22ba4322
Daniel-Digid <daniel@dig.id>
2023-03-23 15:45:44 +0000

    dev

--- template-parts/modules/booking-popup.php
+++ template-parts/modules/booking-popup.php
@@ -1,6 +1,12 @@
 <div class="hidden">
-    <div class="max-w-[950px]" id="open">
-        <h2>Hello</h2>
-        <p>Booking Popup Content</p>
+    <div class="relative max-w-[950px] flex justify-center align-middle lg:p-32 mt-12 border border-solid" id="open" title="cenas">
+        <div class="absolute top-0 left-0 w-full bg-main-red border-b border-solid border-black grid grid-cols-12">
+            <div class="col-span-11">
+                <p class="text-white font-bold py-[9px] px-4 text-2xl uppercase"><?php esc_html_e( 'Jeztz Buchen', 'chasseral' ); ?></p>
+            </div>
+            <div class="col-span-1 border-l border-solid border-black"></div>
+        </div>
+        <p class="text-2xl leading-10 text-center text-black mb-12 max-w-md"><?php esc_html_e( 'Zimmerreservationen können nur telefonisch während der Öffnungszeiten entgegen genommen werden.', 'chasseral' ); ?></p>
+        <p class="text-2xl leading-10 text-center text-black"><b><?php esc_html_e( 'Tel. 032 751 24 51', 'chasseral' ); ?></b></p>
     </div>
 </div>
