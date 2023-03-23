dd00586396190bd08cb7b303ecfd260b024be896
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-23 14:32:04 +0000

    up

--- page-templates/page-hotel.php
+++ page-templates/page-hotel.php
@@ -5,10 +5,10 @@
 
 get_header( 'hotel' );
 	do_action( 'before_main_content' );
-		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section','intro' );
-		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section','blocks' );
-		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section','your-stay' );
-		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section','bankette' );
-		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section','events-list' );
+		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section', 'intro' );
+		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section', 'blocks' );
+		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section', 'your-stay' );
+		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section', 'bankette' );
+		get_template_part( 'template-parts/pages/hotel/hotel-restaurant/section', 'events-list' );
 	do_action( 'after_main_content' );
 get_footer();
