f76cec905be66febef200cbdf93a980c34843030
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-23 14:04:13 +0000

    up

--- template-parts/modules/activities.php
+++ template-parts/modules/activities.php
@@ -1,43 +1,50 @@
 <section class="section-activities px-0 py-10 lg:py-32">
-    <div class="activities-content">
-        <div class="container grid grid-cols-1 lg:grid-cols-2 items-end mx-auto max-w-7xl mb-8 px-8 lg:px-0">
-            <div class="col-span-1">
-                <h2 class="section__title text-black text-3xl font-bold mb-4 lg:mb-0"><?php esc_html_e( 'Aktivitäten', 'chasseral' ); ?></h2>
-            </div>
-            <div class="col-span-1 lg:text-end">
-                <?php 
-                $act_page = get_permalink( get_page_by_path('top-of-jura/aktivitaeten') -> ID ); ?>
-                <a class="btn__arrow-alle" href="<?php echo $act_page; ?>"><?php esc_html_e( 'Alle Aktivitäten', 'chasseral' ); ?> <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M6 12L4.93125 10.95L9.13125 6.75H0V5.25H9.13125L4.93125 1.05L6 0L12 6L6 12Z" fill="black"/>
-                </svg></a>
-            </div>
-        </div>
-    </div>
-    <div class="activities-list">
-        <div class="container grid grid-cols-1 lg:grid-cols-3 gap-4 mx-auto max-w-7xl">
-            <?php
-            $featured_events = get_field( 'activities' );
-            if ( $featured_events ) :
-                foreach ( $featured_events as $post ) :
-                    setup_postdata( $post ); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class( 'col-span-1' ); ?>>
-                    <?php if ( has_post_thumbnail() ) :
-                    echo get_the_post_thumbnail( get_the_ID(), 'activities-thumbnail' );
-                    else : ?>
-                    <img src="https://via.placeholder.com/420x282">
-                    <?php endif; ?>
-                    <div class="activities__content-wrapper p-8 bg-white min-h-[400px] relative">
-                        <h2 class="font-bold text-xl mb-4"><?php the_title(); ?></h2>
-                        <span class="text-sm leading-7"><?php the_excerpt(); ?></span>
-                        <a class="btn__arrow-alle absolute bottom-5" href="<?php the_permalink( $post_id ); ?>"><?php esc_html_e( 'Zur Webseite', 'chasseral' ); ?> <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 12L4.93125 10.95L9.13125 6.75H0V5.25H9.13125L4.93125 1.05L6 0L12 6L6 12Z" fill="black"/>
-                        </svg></a>
-                    </div>
-                </article>
-                <?php endforeach;
-                wp_reset_postdata();
-            endif; ?>
-            
-        </div>
-    </div>
+	<div class="activities-content">
+		<div class="container grid grid-cols-1 lg:grid-cols-2 items-end mx-auto max-w-7xl mb-8 px-8 lg:px-0">
+			<div class="col-span-1">
+				<h2 class="section__title text-black text-3xl font-bold mb-4 lg:mb-0"><?php esc_html_e( 'Aktivitäten', 'chasseral' ); ?></h2>
+			</div>
+			<div class="col-span-1 lg:text-end">
+				<?php
+				$act_page = get_permalink( get_page_by_path( 'top-of-jura/aktivitaeten' ) -> ID ); ?>
+				<a class="btn__arrow-alle" href="<?php echo $act_page; ?>"><?php esc_html_e( 'Alle Aktivitäten', 'chasseral' ); ?>
+					<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M6 12L4.93125 10.95L9.13125 6.75H0V5.25H9.13125L4.93125 1.05L6 0L12 6L6 12Z" fill="black"/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="activities-list">
+		<div class="container grid grid-cols-1 lg:grid-cols-3 gap-4 mx-auto max-w-7xl">
+			<?php
+			$featured_events = get_field( 'activities' );
+			if ( $featured_events ) :
+				foreach ( $featured_events as $post ) :
+					setup_postdata( $post );
+					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'col-span-1' ); ?>>
+						<?php
+						if ( has_post_thumbnail() ) :
+							echo get_the_post_thumbnail( get_the_ID(), 'activities-thumbnail' );
+						else :
+							echo '<img src="https://via.placeholder.com/420x282">';
+						endif;
+						?>
+						<div class="activities__content-wrapper p-8 bg-white min-h-[400px] relative">
+							<h2 class="font-bold text-xl mb-4"><?php the_title(); ?></h2>
+							<span class="text-sm leading-7"><?php the_excerpt(); ?></span>
+							<a class="btn__arrow-alle absolute bottom-5" href="<?php the_permalink( $post_id ); ?>"><?php esc_html_e( 'Zur Webseite', 'chasseral' ); ?>
+								<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M6 12L4.93125 10.95L9.13125 6.75H0V5.25H9.13125L4.93125 1.05L6 0L12 6L6 12Z" fill="black"/>
+								</svg>
+							</a>
+						</div>
+					</article>
+					<?php
+				endforeach;
+				wp_reset_postdata();
+			endif; ?>
+		</div>
+		</div>
 </section>
--- template-parts/pages/jura/top-of-jura/section-intro.php
+++ template-parts/pages/jura/top-of-jura/section-intro.php
@@ -1,16 +1,16 @@
 <section id="section-topofjura" class="section-topofjura__intro h-full lg:h-screen grid grid-cols-1 lg:grid-rows-2">
-    <?php $header_image = get_field('section_intro_header_image');
-    $size = 'chasseral-jura-headers';
-    $header_imgArr = wp_get_attachment_image_src( $header_image, $size ); ?>
-    <div class="bg-cover bg-center h-[50vh] lg:h-full w-full lg:row-span-1" style="background-image: url(<?php echo $header_imgArr[0]; ?>);"></div>
-    <div class="flex justify-center h-full w-full row-span-1 bg-no-repeat p-8" style="background-image: url(<?php echo wp_upload_dir()['url'] . '/jura_illustration.svg' ?>);    background-position: 0 105%;">
-        <div class="pb-36 lg:py-20 container grid grid-cols-1 lg:grid-cols-2 justify-center">
-            <div class="mb-4">
-                <h2 class="title-big"><?php the_field('section_intro_title'); ?></h2>
-            </div>
-            <div>
-                <p class="text-body px-16 lg:px-36"><?php the_field('section_intro_description'); ?></p>
-            </div>
-        </div>
-    </div>
+	<?php $header_image = get_field( 'section_intro_header_image' );
+	$size = 'chasseral-jura-headers';
+	$header_imgArr = wp_get_attachment_image_src( $header_image, $size ); ?>
+	<div class="bg-cover bg-center h-[50vh] lg:h-full w-full lg:row-span-1 border-b border-solid border-black" style="background-image: url(<?php echo $header_imgArr[0]; ?>);"></div>
+	<div class="flex justify-center h-full w-full row-span-1 bg-no-repeat p-8" style="background-image: url(<?php echo wp_upload_dir()['url'] . '/jura_illustration.svg' ?>);    background-position: 0 105%;">
+		<div class="pb-36 lg:py-20 container grid grid-cols-1 lg:grid-cols-2 justify-center">
+			<div class="mb-4">
+				<h2 class="title-big"><?php the_field( 'section_intro_title' ); ?></h2>
+			</div>
+			<div>
+				<p class="text-body px-16 lg:px-36"><?php the_field( 'section_intro_description' ); ?></p>
+			</div>
+		</div>
+	</div>
 </section>
