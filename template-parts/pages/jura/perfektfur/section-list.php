bbdb0685b46ad16503d8dcf0962704100d24ae54
Daniel-Digid <daniel@dig.id>
2023-03-23 17:05:02 +0000

    dev

--- template-parts/pages/jura/perfektfur/section-list.php
+++ template-parts/pages/jura/perfektfur/section-list.php
@@ -6,7 +6,7 @@
         'post_status' => 'publish',
         'posts_per_page' => 8, 
         'orderby' => 'date',
-        'order' => 'ASC',
+        'order' => 'DESC',
     );
     $loop = new WP_Query( $args ); 
     while ( $loop->have_posts() ) : $loop->the_post(); ?>
