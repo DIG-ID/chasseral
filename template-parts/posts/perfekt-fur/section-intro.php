658656793e97244bcb27610c7006a9d00e57ebc3
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-23 20:53:39 +0000

    up

--- template-parts/posts/perfekt-fur/section-intro.php
+++ template-parts/posts/perfekt-fur/section-intro.php
@@ -1,5 +1,5 @@
-<section id="single-perfekt-fur-intro" class="section-perfekt-fur__intro grid grid-cols-1">
-	<div class="w-full chasseral__col">
+<section id="single-perfekt-fur-intro" class="section-perfekt-fur__intro grid grid-cols-1 border-t border-solid border-black">
+	<div class="w-full chasseral__col border-b border-solid border-black">
 		<?php
 		if ( has_post_thumbnail() ) :
 			the_post_thumbnail( 'chasseral-jura-headers' );
@@ -8,7 +8,7 @@
 		endif;
 		?>
 		</div>
-		<div class="p-8 lg:pt-40 lg:pb-20 container max-w-7xl mx-auto">
+		<div class="p-8 lg:pt-20 lg:pb-10 container max-w-7xl mx-auto">
 			<div class="mb-8 lg:mb-20 w-full">
 				<h2 class="title-big"><?php the_title(); ?></h2>
 			</div>
