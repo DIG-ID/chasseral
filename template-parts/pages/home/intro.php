de0bcdd35659189f1336bc2603101469ff055213
Daniel-Digid <daniel@dig.id>
2023-03-27 16:21:28 +0100

    dev

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,9 +1,9 @@
 <section class="section-intro py-16 lg:py-32 border-b border-solid overflow-hidden">
-	<div class="container mx-auto px-8 grid grid-cols-1 lg:grid-cols-12">
-		<div class="col-span-1 lg:col-span-4">
+	<div class="lg:container mx-auto px-8 grid grid-cols-1 md:grid-cols-12">
+		<div class="col-span-1 md:col-span-5 lg:col-span-4">
 			<h2 class="title-normal !font-normal"><?php the_field( 'intro_title' ); ?></h2>
 		</div>
-		<div class="col col-span-1 lg:col-span-5 lg:col-start-6">
+		<div class="col col-span-1 md:col-span-6 lg:col-span-5 md:col-start-7 lg:col-start-6">
 			<p class="text-body"><?php the_field( 'intro_description' ); ?></p>
 		</div>
 	</div>
