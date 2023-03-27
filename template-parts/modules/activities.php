c7e87475ca1e16d93e1595c9e2e005b1ef3d0f2f
Daniel-Digid <daniel@dig.id>
2023-03-27 18:20:54 +0100

    dev

--- template-parts/modules/activities.php
+++ template-parts/modules/activities.php
@@ -10,7 +10,7 @@
 		</div>
 	</div>
 	<div class="activities-list">
-		<div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto max-w-7xl">
+		<div class="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mx-auto max-w-7xl">
 			<?php
 			$featured_events = get_field( 'activities' );
 			if ( $featured_events ) :
