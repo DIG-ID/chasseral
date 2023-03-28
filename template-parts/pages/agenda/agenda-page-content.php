f981c2dcb33b6402ec8766835e5d2c63dea57c59
Daniel-Digid <daniel@dig.id>
2023-03-28 18:43:37 +0100

    dev

--- template-parts/pages/agenda/agenda-page-content.php
+++ template-parts/pages/agenda/agenda-page-content.php
@@ -1,7 +1,7 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'overflow-hidden border-t border-solid border-black py-20 lg:pb-8 xl:pb-20' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'overflow-hidden border-t border-solid border-black pt-20 pb-0 lg:pb-8 xl:pb-20' ); ?>>
 	<div class="container mx-auto px-8">
 		<h1 class="title-normal !mb-0"><?php the_title(); ?></h1>
-		<section class="section-agenda-list py-20">
+		<section class="section-agenda-list py-8 lg:py-20">
 			<?php get_template_part( 'template-parts/loops/loop', 'agenda' ); ?>
 		</section>
 	</div>
