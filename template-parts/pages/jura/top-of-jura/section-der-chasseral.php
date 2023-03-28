ead5d1e92f56c0c2d73c69c129784afb1c32288a
Daniel-Digid <daniel@dig.id>
2023-03-28 15:08:31 +0100

    dev

--- template-parts/pages/jura/top-of-jura/section-der-chasseral.php
+++ template-parts/pages/jura/top-of-jura/section-der-chasseral.php
@@ -1,11 +1,11 @@
 <section class="section-2cols-image grid grid-cols-1 lg:grid-cols-2 border-y border-solid">
-    <div class="flex justify-center p-8 lg:p-20 flex-col">
+    <div class="flex justify-center p-8 lg:p-20 pb-24 lg:pb-20 flex-col order-2 lg:order-1">
         <h2 class="title-normal"><?php the_field('section_der_chasseral_title'); ?></h2>
         <p class="text-black text-sm font-normal leading-7 mb-14 xl:max-w-[450px]"><?php the_field('section_der_chasseral_description'); ?></p>
         <?php $page = get_permalink( get_page_by_path('top-of-jura/chasseral') -> ID ); ?>
         <a href="<?php echo $page; ?>" class="btn-general btn-general--jura"><?php echo esc_html_e( 'Mehr erfahren', 'chasseral' ) ?></a>
     </div>
-    <div class="bg-cover bg-center h-full w-full chasseral__col border-l border-solid">
+    <div class="bg-cover bg-center h-full w-full chasseral__col border-l border-solid order-1 lg:order-2">
         <?php $b_image = get_field('section_der_chasseral_image'); ?>
         <?php echo wp_get_attachment_image( $b_image, 'section-column-img' ); ?>
     </div>
