1d836f74104ef0c9cd579f3fbb381356d7ecd1ce
Daniel-Digid <daniel@dig.id>
2023-03-28 18:55:32 +0100

    dev

--- template-parts/posts/der-jura/der-jura-single-content.php
+++ template-parts/posts/der-jura/der-jura-single-content.php
@@ -1,11 +1,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'overflow-hidden border-t border-solid border-black' ); ?>>
 	<?php
 	if ( has_post_thumbnail() ) :
-		?><div class="h-[80vh] border-b border-solid border-black bg-center bg-no-repeat bg-cover bg-fixed" style="background-image: url(<?php echo esc_url( get_the_post_thumbnail_url( get_the_ID(), 'full' ) ); ?>)"></div>
+		?><div class="h-[40vh] xl:h-[80vh] border-b border-solid border-black bg-center bg-no-repeat bg-cover bg-fixed" style="background-image: url(<?php echo esc_url( get_the_post_thumbnail_url( get_the_ID(), 'full' ) ); ?>)"></div>
 		<?php
 	endif;
 	?>
-	<div class="container mx-auto px-8 py-36">
+	<div class="container mx-auto px-8 py-16 xl:py-36">
 		<div class="post-content-wrapper">
 			<h1 class="title-normal"><?php the_title(); ?></h1>
 			<div class="post-content">
