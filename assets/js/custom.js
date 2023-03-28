782266ade6f4107b4c6c8999ab3779a93b481779
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-28 20:53:26 +0100

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,9 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   //wait until images, links, fonts, stylesheets, and js is loaded
   window.addEventListener("load", () => {
-    if (document.body.classList.contains("page-template-page-home")) {
-
-    }
-
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }, false);
 });
