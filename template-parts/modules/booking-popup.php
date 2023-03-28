456549ba2397eadc4f190253d8a33c9a1d1195da
Daniel-Digid <daniel@dig.id>
2023-03-28 22:50:20 +0100

    dev

--- template-parts/modules/booking-popup.php
+++ template-parts/modules/booking-popup.php
@@ -1,12 +1,12 @@
 <div class="hidden">
-    <div class="relative max-w-[950px] flex justify-center align-middle lg:p-32 mt-12 border border-solid" id="open" title="cenas">
+    <div class="relative max-w-[950px] flex justify-center align-middle pt-24 pb-16 px-8 lg:p-32 mt-12 border border-solid" id="open" title="cenas">
         <div class="absolute top-0 left-0 w-full bg-main-red border-b border-solid border-black grid grid-cols-12">
-            <div class="col-span-11">
+            <div class="col-span-10 md:col-span-11">
                 <p class="text-white font-bold py-[9px] px-4 text-2xl uppercase"><?php esc_html_e( 'Jeztz Buchen', 'chasseral' ); ?></p>
             </div>
-            <div class="col-span-1 border-l border-solid border-black"></div>
+            <div class="col-span-2 md:col-span-1 border-l border-solid border-black"></div>
         </div>
-        <p class="text-2xl leading-10 text-center text-black mb-12 max-w-md"><?php esc_html_e( 'Zimmerreservationen können nur telefonisch während der Öffnungszeiten entgegen genommen werden.', 'chasseral' ); ?></p>
-        <p class="text-2xl leading-10 text-center text-black"><b><?php esc_html_e( 'Tel. 032 751 24 51', 'chasseral' ); ?></b></p>
+        <p class="text-lg md:text-2xl leading-10 text-center text-black mb-12 max-w-md"><?php esc_html_e( 'Zimmerreservationen können nur telefonisch während der Öffnungszeiten entgegen genommen werden.', 'chasseral' ); ?></p>
+        <p class="text-lg md:text-2xl leading-10 text-center text-black"><b><?php esc_html_e( 'Tel. 032 751 24 51', 'chasseral' ); ?></b></p>
     </div>
 </div>
