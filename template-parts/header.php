92c3ce5e5baf8e0f2e6c442509cad18d062da443
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-29 12:27:35 +0100

    up

--- template-parts/header.php
+++ template-parts/header.php
@@ -4,6 +4,14 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<?php wp_head(); ?>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-EK7971B935"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-EK7971B935');
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
