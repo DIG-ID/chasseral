7f610084bcec6d27cf685ca29262f2cacae88d8f
Daniel-Digid <daniel@dig.id>
2023-03-30 12:08:26 +0100

    dev

--- template-parts/pages/404/content.php
+++ template-parts/pages/404/content.php
@@ -1,7 +1,7 @@
-<section class="section-not-found pt-20 pb-64 bg-contain bg-bottom bg-no-repeat" style="background-image: url(<?php echo esc_url( get_template_directory_uri() . '/assets/images/chasseral-mountains.min.svg' ) ?>);background-position-y: 140%;">
-	<div class="container mx-auto px-2">
+<section class="section-not-found pt-8 pb-16 lg:pt-20 lg:pb-24 xl:pb-64 px-8 md:px-16 xl:px-0 bg-contain bg-bottom bg-no-repeat bg-opacity-0 lg:bg-opacity-100" style="background-image: url(<?php echo esc_url( get_template_directory_uri() . '/assets/images/chasseral-mountains.min.svg' ) ?>);background-position-y: 140%;">
+	<div class="container mx-auto lg:px-2">
 		<h1 class="title-normal !mb-4"><br><?php esc_html_e( 'Seite nicht gefunden!', 'chasseral' ); ?></h1>
-		<p class="title-big !text-[200px]"><?php esc_html_e( '404', 'chasseral' ); ?></p>
+		<p class="title-big !text-7xl md:!text-9xl lg:!text-[200px]"><?php esc_html_e( '404', 'chasseral' ); ?></p>
 		<p class="text-2xl font-semibold"><?php esc_html_e( 'Die von Ihnen gesuchte Seite existiert nicht.', 'chasseral' ); ?></p>
 		<p class="text-body !mb-10"><?php esc_html_e( 'Sie können auf die Schaltfläche unten klicken, um zur Homepage zurückzukehren.', 'chasseral' ); ?></p>
 		<a class="btn-general hover:bg-white" href="<?php echo esc_url( get_site_url( 'https' ) ); ?>"><?php esc_html_e( 'Zur Website gehen', 'chasseral' ); ?></a>
