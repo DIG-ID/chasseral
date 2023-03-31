206c7db6b10f1da4adb022e89f07d7ba0514f27f
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-03-31 11:33:51 +0100

    up

--- template-parts/modules/widget-mobile.php
+++ template-parts/modules/widget-mobile.php
@@ -1,3 +1,3 @@
-<div class="mobile-widget fixed t-[75px] left-0 w-full border-b border-solid border-black z-20 bg-bg-color md:hidden" >
+<div class="mobile-widget fixed top-[75px] left-0 w-full border-b border-solid border-black z-20 bg-bg-color md:hidden" >
 	<?php do_action( 'service_status_widget', false, true ); ?>
 </div>
